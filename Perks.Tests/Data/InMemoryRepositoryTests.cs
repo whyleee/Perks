5f4ba937815cafc7489ab76e0597225393d7e701
whyleee <devnull@localhost>
2012-11-25 16:24:50 +0200

    Added IRepository with in-memory implementation and unit tests

---
+++ Perks.Tests/Data/InMemoryRepositoryTests.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using Ninject;
+using Perks.Data;
+
+namespace Perks.Tests.Data
+{
+    public class InMemoryRepositoryTests : FixtureWithKernel
+    {
+        public class Customer
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+        }
+
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+        }
+
+        public class Book
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public string Author { get; set; }
+        }
+
+        public class Caller
+        {
+            public string Phone { get; set; }
+        }
+
+        private IRepository<T> CreateRepo<T>() where T : class
+        {
+            return kernel.Get<InMemoryRepository<T>>();
+        }
+
+        private IRepository<Customer> CreateRepoWithBobAndJames()
+        {
+            var repo = CreateRepo<Customer>();
+
+            var bob = new Customer {Name = "Bob", Surname = "Marley"};
+            var james = new Customer {Name = "James", Surname = "Bond"};
+
+            repo.Save(bob);
+            repo.Save(james);
+
+            return repo;
+        }
+        
+        [Test]
+        public void GetAll_Should_return_all_repo_items()
+        {
+            // setups
+            var repo = CreateRepoWithBobAndJames();
+
+            // act
+            var all = repo.GetAll();
+
+            // asserts
+            all.Should().HaveCount(2);
+            all.Should().Contain(x => x.Name == "Bob");
+            all.Should().Contain(x => x.Name == "James");
+        }
+
+        [Test]
+        public void GetAll_When_repo_is_empty_Should_return_empty_collection()
+        {
+            // setups
+            var repo = CreateRepo<Customer>();
+
+            // act
+            var all = repo.GetAll();
+
+            // asserts
+            all.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Get_Should_return_repo_item_by_id()
+        {
+            // setups
+            var repo = CreateRepoWithBobAndJames();
+
+            // act
+            var customer = repo.Get("James");
+
+            // asserts
+            customer.Should().NotBeNull();
+            customer.Name.Should().Be("James");
+        }
+
+        [Test]
+        public void Get_When_item_is_not_found_Should_return_null()
+        {
+            // setups
+            var repo = CreateRepoWithBobAndJames();
+
+            // act
+            var customer = repo.Get("John");
+
+            // asserts
+            customer.Should().BeNull();
+        }
+
+        [Test]
+        public void Get_For_items_with_int_id_Should_work()
+        {
+            // setups
+            var repo = CreateRepo<Product>();
+
+            var table = new Product {Id = 1, Name = "Table", Price = 39.99M};
+            var chair = new Product {Id = 2, Name = "Chair", Price = 14.59M};
+
+            repo.Save(table);
+            repo.Save(chair);
+
+            // act
+            var product = repo.Get(2);
+
+            // asserts
+            product.Should().Be(chair);
+        }
+
+        [Test]
+        public void Get_For_items_with_guid_id_Should_work()
+        {
+            // setups
+            var repo = CreateRepo<Book>();
+
+            var cplusplusBookId = Guid.NewGuid();
+            var csharpBookId = Guid.NewGuid();
+
+            var cplusplusBook = new Book {Id = cplusplusBookId, Author = "Deitel", Name = "How to program on C++"};
+            var csharpBook = new Book {Id = csharpBookId, Author = "Nagel", Name = "Professional C# and .NET"};
+
+            repo.Save(cplusplusBook);
+            repo.Save(csharpBook);
+
+            // act
+            var product = repo.Get(csharpBookId);
+
+            // asserts
+            product.Should().Be(csharpBook);
+        }
+
+        [Test]
+        public void Get_For_items_with_phone_Should_work()
+        {
+            // setups
+            var repo = CreateRepo<Caller>();
+
+            var bob = new Caller {Phone = "11223344"};
+            var james = new Caller {Phone = "87654321"};
+
+            repo.Save(bob);
+            repo.Save(james);
+
+            // act
+            var caller = repo.Get("11223344");
+
+            // asserts
+            caller.Should().Be(bob);
+        }
+
+        [Test]
+        public void Save_For_new_item_Should_add_it_to_the_repo()
+        {
+            // setups
+            var repo = CreateRepoWithBobAndJames();
+
+            // act
+            var john = new Customer {Name = "John", Surname = "Smith"};
+            repo.Save(john);
+
+            // asserts
+            repo.GetAll().Should().HaveCount(3);
+            repo.Get("John").Should().Be(john);
+        }
+
+        [Test]
+        public void Save_For_existing_item_Should_update_it_in_the_repo()
+        {
+            // setups
+            var repo = CreateRepoWithBobAndJames();
+
+            // act
+            var existing = repo.Get("James");
+            existing.Surname = "Smith";
+
+            repo.Save(existing);
+
+            // asserts
+            repo.GetAll().Should().HaveCount(2);
+            repo.Get("James").Surname.Should().Be("Smith");
+        }
+
+        [Test]
+        public void Save_For_existing_item_When_id_is_changed_Should_delete_old_item_and_add_new_item_to_the_repo()
+        {
+            // setups
+            var repo = CreateRepoWithBobAndJames();
+
+            // act
+            var existing = repo.Get("James");
+            existing.Name = "John";
+
+            repo.Save(existing);
+
+            // asserts
+            repo.GetAll().Should().HaveCount(2);
+            repo.Get("James").Should().BeNull();
+            repo.Get("John").Should().Be(existing);
+        }
+
+        [Test]
+        public void Delete_Should_delete_the_item_from_the_repo()
+        {
+            // setups
+            var repo = CreateRepoWithBobAndJames();
+
+            // act
+            var bob = repo.Get("Bob");
+            repo.Delete(bob);
+
+            // asserts
+            repo.GetAll().Should().HaveCount(1);
+            repo.Get("Bob").Should().BeNull();
+        }
+
+        [Test]
+        public void Delete_For_not_exising_item_Should_not_do_anything()
+        {
+            // setups
+            var repo = CreateRepoWithBobAndJames();
+
+            // act
+            var john = new Customer { Name = "John", Surname = "Smith" };
+            repo.Delete(john);
+
+            // asserts
+            repo.GetAll().Should().HaveCount(2);
+        }
+    }
+}
---
+++ Perks/Data/InMemoryRepository.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Perks.Data
+{
+    /// <summary>
+    /// Simple in-memory repository, which using <see cref="List{T}"/> internally and doesn't provide persistance for data.
+    /// </summary>
+    /// <typeparam name="T">The type of items in repository.</typeparam>
+    public class InMemoryRepository<T> : IRepository<T> where T : class
+    {
+        private readonly List<T> _list = new List<T>();
+
+        /// <summary>
+        /// Gets all items of the specified type from the storage.
+        /// </summary>
+        /// <returns>
+        /// All items of the specified type from the storage.
+        /// </returns>
+        public IQueryable<T> GetAll()
+        {
+            return _list.AsReadOnly().AsQueryable();
+        }
+
+        /// <summary>
+        /// Gets the item from the storage by ID.
+        /// </summary>
+        /// <param name="id">The ID of the item.</param>
+        /// <returns>
+        /// The item from the storage by ID.
+        /// </returns>
+        public T Get(object id)
+        {
+            return _list.FirstOrDefault(x => GetId(x).Equals(id));
+        }
+
+        /// <summary>
+        /// Adds the specified item to the storage or updates existing item.
+        /// </summary>
+        /// <param name="item">The item to add.</param>
+        public void Save(T item)
+        {
+            if (_list.Any(x => EqualsById(x, item)))
+            {
+                Delete(item);
+            }
+
+            _list.Add(item);
+        }
+
+        /// <summary>
+        /// Deletes the specified item.
+        /// </summary>
+        /// <param name="item">The item to delete.</param>
+        public void Delete(T item)
+        {
+            var itemInRepo = _list.FirstOrDefault(x => EqualsById(x, item));
+
+            if (itemInRepo != null)
+            {
+                _list.Remove(itemInRepo);
+            }
+        }
+
+        /// <summary>
+        /// Determines whether provided items are equal by ID.
+        /// </summary>
+        /// <param name="item1">First item.</param>
+        /// <param name="item2">Second item.</param>
+        /// <returns><c>true</c> if item IDs are equal; otherwise - <c>false</c>.</returns>
+        private bool EqualsById(object item1, object item2)
+        {
+            return GetId(item1).Equals(GetId(item2));
+        }
+
+        /// <summary>
+        /// Gets item ID, trying to identificate it.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns>Item ID if found, or its hash code otherwise.</returns>
+        private object GetId(object item)
+        {
+            var possibleIdPropertyNames = new[] {"Id", "Name", "Phone"};
+
+            foreach (var idPropertyName in possibleIdPropertyNames)
+            {
+                var idProperty = item.GetType().GetProperty(idPropertyName, BindingFlags.Public | BindingFlags.Instance);
+
+                if (idProperty != null)
+                {
+                    return idProperty.GetValue(item, null);
+                }
+            }
+
+            return item.GetHashCode();
+        }
+    }
+}
