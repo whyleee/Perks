10f19c5015276675d7a83665c03351b1647d4378
whyleee <devnull@localhost>
2013-05-06 23:00:01 +0300

    Version set to 1.3, switched to .NET Framework 4.5

--- Perks/Properties/AssemblyInfo.cs
+++ Perks/Properties/AssemblyInfo.cs
@@ -1,36 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Info about this particular assembly
 [assembly: AssemblyTitle("Perks")]
 [assembly: AssemblyDescription("Different helpers and wrappers for .NET, based on dependency injection, thus fully testable")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Pavel Nezhencev")]
-[assembly: AssemblyProduct("Perks")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7582ff8f-aef8-4ef3-af70-26b5a3531f09")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
