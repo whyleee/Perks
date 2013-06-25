e8844893f071195051af0a0fc774beeb0ab7c6ba
whyleee <devnull@localhost>
2013-06-25 03:04:55 +0300

    Added .nuspec for Perks.Mvc

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -9,3 +9,4 @@ using System.Runtime.InteropServices;
 // Product version
 [assembly: AssemblyVersion("1.3.5.0")]
 [assembly: AssemblyFileVersion("1.3.5.0")]
+[assembly: AssemblyInformationalVersion("1.3.5-beta")]
